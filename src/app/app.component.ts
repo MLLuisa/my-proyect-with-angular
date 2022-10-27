34735653263818744aa96c074c9879e2010148bd
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-10-27 16:54:16 +0200

    Added string interpolation with some examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-project-angular';
+  name =  'Maria';
+  age = 20;
+  image = 'https://via.placeholder.com/150';
 }
