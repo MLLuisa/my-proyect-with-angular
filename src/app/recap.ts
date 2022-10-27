04649f5e5a84133d3341362c129e17c51a53492e
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-10-27 16:33:29 +0200

    Added one file of typescript with base concepts

---
+++ src/app/recap.ts
@@ -0,0 +1,15 @@
+const username: string = 'MLLuisa';
+
+const sum = (a: number, b: number) => {
+  return a + b;
+}
+
+sum(2, 2);
+
+class Person {
+  constructor(public age: number, public lastName: string) {
+  }
+}
+
+const mariaLuisa = new Person(27, 'Lufra');
+
