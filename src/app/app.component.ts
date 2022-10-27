7efd51bb347f1b5fc1288d142bb34a106ee2d5a6
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-10-27 17:23:51 +0200

    Added example of property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,10 @@ export class AppComponent {
   name =  'Maria';
   age = 20;
   image = 'https://via.placeholder.com/150';
+  btnDisable = true;
+  person = {
+    name: 'Maria',
+    age: 20,
+    avatar: 'https://via.placeholder.com/150'
+  }
 }
