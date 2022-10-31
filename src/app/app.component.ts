0e639dc58b2342fef3aa9709195e09410a16bfb0
Maria Luisa Lufrano <marialuisa.lufrano@icloud.com>
2022-10-31 16:49:49 +0100

    Added exercise with dinamic styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg = 10;
   name =  'Maria';
   age = 20;
   image = 'https://via.placeholder.com/150';
